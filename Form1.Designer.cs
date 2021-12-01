7192c3b32b20451c5d9cf3949c287585035ac742
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-12-01 10:08:04 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Moving_an_Image
+{
+    partial class FormMoveImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgPacMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPacMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgPacMan
+            // 
+            this.imgPacMan.BackColor = System.Drawing.Color.Transparent;
+            this.imgPacMan.Image = global::Moving_an_Image.Properties.Resources.PacRight;
+            this.imgPacMan.Location = new System.Drawing.Point(280, 204);
+            this.imgPacMan.Name = "imgPacMan";
+            this.imgPacMan.Size = new System.Drawing.Size(44, 40);
+            this.imgPacMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgPacMan.TabIndex = 0;
+            this.imgPacMan.TabStop = false;
+            // 
+            // FormMoveImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgPacMan);
+            this.Name = "FormMoveImage";
+            this.Text = "Move Image";
+            this.Load += new System.EventHandler(this.FormMoveImage_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMoveImage_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPacMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgPacMan;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moving_an_Image
+{
+    public partial class FormMoveImage : System.Windows.Forms.Form
+    {
+        int speed;
+        public FormMoveImage()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMoveImage_Load(object sender, EventArgs e)
+        {
+            speed = 5;
+        }
+
+        private void FormMoveImage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+                imgPacMan.Left += speed;
+            else if (e.KeyCode == Keys.Left)
+                imgPacMan.Left -= speed;
+            else if (e.KeyCode == Keys.Up)
+                imgPacMan.Top -= speed;
+            else if (e.KeyCode == Keys.Down)
+                imgPacMan.Top += speed;
+        }
+
+        
+    }
+}
