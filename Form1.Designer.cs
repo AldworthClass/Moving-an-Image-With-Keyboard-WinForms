75c58f6c8878fe83279784bf1d146ef7ded1b4ce
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-12-13 10:07:03 -0500

    collision done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,21 @@
         {
             this.imgPacMan = new System.Windows.Forms.PictureBox();
             this.imgCookie = new System.Windows.Forms.PictureBox();
+            this.imgRock = new System.Windows.Forms.PictureBox();
+            this.imgRock3 = new System.Windows.Forms.PictureBox();
+            this.imgRock2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.imgPacMan)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgCookie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock2)).BeginInit();
             this.SuspendLayout();
             // 
             // imgPacMan
             // 
             this.imgPacMan.BackColor = System.Drawing.Color.Transparent;
             this.imgPacMan.Image = global::Moving_an_Image.Properties.Resources.PacRight;
-            this.imgPacMan.Location = new System.Drawing.Point(280, 204);
+            this.imgPacMan.Location = new System.Drawing.Point(295, 207);
             this.imgPacMan.Name = "imgPacMan";
             this.imgPacMan.Size = new System.Drawing.Size(44, 40);
             this.imgPacMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -47,27 +53,64 @@
             // 
             // imgCookie
             // 
+            this.imgCookie.BackColor = System.Drawing.Color.Transparent;
             this.imgCookie.Image = global::Moving_an_Image.Properties.Resources.Cookie;
-            this.imgCookie.Location = new System.Drawing.Point(378, 204);
+            this.imgCookie.Location = new System.Drawing.Point(209, 89);
             this.imgCookie.Name = "imgCookie";
-            this.imgCookie.Size = new System.Drawing.Size(35, 33);
+            this.imgCookie.Size = new System.Drawing.Size(30, 28);
             this.imgCookie.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgCookie.TabIndex = 1;
             this.imgCookie.TabStop = false;
+            // 
+            // imgRock
+            // 
+            this.imgRock.Image = global::Moving_an_Image.Properties.Resources.rock;
+            this.imgRock.Location = new System.Drawing.Point(501, 166);
+            this.imgRock.Name = "imgRock";
+            this.imgRock.Size = new System.Drawing.Size(98, 81);
+            this.imgRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgRock.TabIndex = 2;
+            this.imgRock.TabStop = false;
+            // 
+            // imgRock3
+            // 
+            this.imgRock3.Image = global::Moving_an_Image.Properties.Resources.rock;
+            this.imgRock3.Location = new System.Drawing.Point(130, 244);
+            this.imgRock3.Name = "imgRock3";
+            this.imgRock3.Size = new System.Drawing.Size(98, 81);
+            this.imgRock3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgRock3.TabIndex = 3;
+            this.imgRock3.TabStop = false;
+            // 
+            // imgRock2
+            // 
+            this.imgRock2.Image = global::Moving_an_Image.Properties.Resources.rock;
+            this.imgRock2.Location = new System.Drawing.Point(362, 51);
+            this.imgRock2.Name = "imgRock2";
+            this.imgRock2.Size = new System.Drawing.Size(98, 81);
+            this.imgRock2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgRock2.TabIndex = 4;
+            this.imgRock2.TabStop = false;
             // 
             // FormMoveImage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.imgCookie);
+            this.Controls.Add(this.imgRock2);
+            this.Controls.Add(this.imgRock3);
+            this.Controls.Add(this.imgRock);
             this.Controls.Add(this.imgPacMan);
+            this.Controls.Add(this.imgCookie);
             this.Name = "FormMoveImage";
             this.Text = "Move Image";
             this.Load += new System.EventHandler(this.FormMoveImage_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMoveImage_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.imgPacMan)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgCookie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgRock2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,6 +119,9 @@
 
         private System.Windows.Forms.PictureBox imgPacMan;
         private System.Windows.Forms.PictureBox imgCookie;
+        private System.Windows.Forms.PictureBox imgRock;
+        private System.Windows.Forms.PictureBox imgRock3;
+        private System.Windows.Forms.PictureBox imgRock2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace Moving_an_Image
     public partial class FormMoveImage : System.Windows.Forms.Form
     {
         int speed;
+        List<PictureBox> rocks = new List<PictureBox>();
         public FormMoveImage()
         {
             InitializeComponent();
@@ -21,33 +22,121 @@ namespace Moving_an_Image
         private void FormMoveImage_Load(object sender, EventArgs e)
         {
             speed = 5;
+            rocks.Add(imgRock);
+            rocks.Add(imgRock2);
+            rocks.Add(imgRock3);
         }
 
         private void FormMoveImage_KeyDown(object sender, KeyEventArgs e)
         {
+            Rectangle tempRect;
+
             if (e.KeyCode == Keys.Right)
+            {               
+                //Checks for eating a cookie
+                if (imgPacMan.Bounds.Contains(imgCookie.Bounds))
+                    imgCookie.Image = Properties.Resources.crumbs;
+
+                
+
                 if (imgPacMan.Right + speed > this.ClientSize.Width)
                     imgPacMan.Left = this.ClientSize.Width - imgPacMan.Width;
-                else
-                    imgPacMan.Left += speed;
+
+                else // Pacman will stay on the Form
+                {
+                    tempRect = imgPacMan.Bounds;
+                    tempRect.X += speed;
+                    foreach (PictureBox rock in rocks)
+                    {
+                        if (tempRect.IntersectsWith(rock.Bounds))
+                        {
+                            tempRect.X = rock.Left - tempRect.Width;
+                        }
+                    }                   
+                    imgPacMan.Bounds = tempRect;
+                }
+                    
+
+                
+                
+
+            }
+                
 
             else if (e.KeyCode == Keys.Left)
+            {
+                //Checks for eating a cookie
+                if (imgPacMan.Bounds.Contains(imgCookie.Bounds))
+                    imgCookie.Image = Properties.Resources.crumbs;
+
                 if (imgPacMan.Left - speed < 0)
                     imgPacMan.Left = 0;
                 else
-                    imgPacMan.Left -= speed;
+                {
+                    tempRect = imgPacMan.Bounds;
+                    tempRect.X -= speed;
+                    foreach (PictureBox rock in rocks)
+                    {
+                        if (tempRect.IntersectsWith(rock.Bounds))
+                        {
+                            tempRect.X = rock.Right;
+                        }
+                    }            
+                    imgPacMan.Bounds = tempRect;
+                }
+            }
+                
 
             else if (e.KeyCode == Keys.Up)
+            {
+                //Checks for eating a cookie
+                if (imgPacMan.Bounds.Contains(imgCookie.Bounds))
+                    imgCookie.Image = Properties.Resources.crumbs;
+
                 if (imgPacMan.Top - speed < 0)
                     imgPacMan.Top = 0;
                 else
-                    imgPacMan.Top -= speed;
+                {
+                    tempRect = imgPacMan.Bounds;
+                    tempRect.Y -= speed;
+                    foreach (PictureBox rock in rocks)
+                    {
+                        if (tempRect.IntersectsWith(rock.Bounds))
+                        {
+                            tempRect.Y = rock.Bottom;
+                        }
+                    }
+                    imgPacMan.Bounds = tempRect;
+                }
+                    
+            }
+                
        
             else if (e.KeyCode == Keys.Down)
+            {
+                //Checks for eating a cookie
+                if (imgPacMan.Bounds.Contains(imgCookie.Bounds))
+                    imgCookie.Image = Properties.Resources.crumbs;
+
                 if (imgPacMan.Bottom + speed > this.ClientSize.Height)
                     imgPacMan.Top = this.ClientSize.Height - imgPacMan.Height;
                 else
-                    imgPacMan.Top += speed;
+                {
+                    tempRect = imgPacMan.Bounds;
+                    tempRect.Y += speed;
+                    foreach (PictureBox rock in rocks)
+                    {
+                        if (tempRect.IntersectsWith(rock.Bounds))
+                        {
+                            tempRect.Y = rock.Top - tempRect.Height;
+                        }
+                    }
+                    imgPacMan.Bounds = tempRect;
+                }
+                 
+
+            }
+                
         }
 
         
